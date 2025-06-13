4b2b6ed414518f022a4eb48a478be219da7fee40
paul <pauloneq@gmail.com>
2025-06-13 23:29:48 +0100

    Update dracula plot

--- plot_collate.py
+++ plot_collate.py
@@ -7,8 +7,8 @@ df['rank'] = df['word_frequency'].rank(ascending=False,method='max')
 
 df['inverse_rank'] = 1 / df['rank']
 
-#scatplot = df.plot.scatter(x='word_frequency',y='inverse_rank',figsize=[12, 6],grid=True)
+scatplot = df.plot.scatter(x='word_frequency',y='inverse_rank',figsize=[12, 6],grid=True)
 
-#fig = scatplot.get_figure()
+fig = scatplot.get_figure()
 
-#fig.savefig('results/jane_eyre.png')
+fig.savefig('results/jane_eyre.png')
