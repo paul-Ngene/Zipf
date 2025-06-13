9d9671a0b8e148dfe1db391c55eb798aa437fc2c
paul <pauloneq@gmail.com>
2025-06-13 21:54:59 +0100

    Updated plotcounts

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -15,15 +15,15 @@ def main(args):
 		xlim=args.xlim)
 	ax.figure.savefig(args.outfile)
 if __name__ == '__main__':
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('infile', type=argparse.FileType('r'),
-nargs='?', default='-',
-help='Word count csv file name')
-parser.add_argument('--outfile', type=str,
-default='plotcounts.png',
-help='Output image file name')
-parser.add_argument('--xlim', type=float, nargs=2,
-metavar=('XMIN', 'XMAX'),
-default=None, help='X-axis limits')
-args = parser.parse_args()
-main(args)
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument('infile', type=argparse.FileType('r'),
+	nargs='?', default='-',
+	help='Word count csv file name')
+	parser.add_argument('--outfile', type=str,
+	default='plotcounts.png',
+	help='Output image file name')
+	parser.add_argument('--xlim', type=float, nargs=2,
+	metavar=('XMIN', 'XMAX'),
+	default=None, help='X-axis limits')
+	args = parser.parse_args()
+	main(args)
